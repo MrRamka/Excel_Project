39c77c248ab65a1b90c81ff4d7b54d5ca5794c61
Ramil <ramil.minyukov@yandex.ru>
2018-12-05 20:50:22 +0300

    Second commit "Hello World"

--- src/com/yabcompany/Main.java
+++ src/com/yabcompany/Main.java
@@ -2,6 +2,7 @@ package com.yabcompany;
 
 public class Main {
 	//Ку-ку епта :)
+    //Hfcbv gblh
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello World");
