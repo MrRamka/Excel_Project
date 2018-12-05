69c02e42f89a29033af75a1d770a27033361eee5
Rasim <rasim.2@bk.ru>
2018-12-05 20:25:28 +0300

    Update Main.java

--- src/com/yabcompany/Main.java
+++ src/com/yabcompany/Main.java
@@ -1,7 +1,7 @@
 package com.yabcompany;
 
 public class Main {
-
+	//Ку-ку епта :)
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello World");
