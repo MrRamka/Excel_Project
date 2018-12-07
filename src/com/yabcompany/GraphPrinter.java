5884fb6b8a231ba3c59fa3def10f1b76a37ea7cf
MrRamka <41204824+mrramka@users.noreply.github.com>
2018-12-08 00:19:11 +0300

    Added config.properties

--- src/com/yabcompany/GraphPrinter.java
+++ src/com/yabcompany/GraphPrinter.java
@@ -1,12 +1,55 @@
 package com.yabcompany;
 
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
 public class GraphPrinter {
+    private static int width;
+    private static int height;
+    private static String bgColor;
+    private static String textColor;
+    private static String[] colors;
+
+    public static String getBgColor() {
+        return bgColor;
+    }
+
+    private static void initProperties(){
+        PropertiesScanner.setProperties();
+        width = PropertiesScanner.getWidth();
+        height = PropertiesScanner.getHeight();
+        bgColor = PropertiesScanner.getBgColor();
+        textColor = PropertiesScanner.getTextColor();
+        colors = PropertiesScanner.getColors();
+    }
+
+    public static void linearGraph() {
+
+    }
+
+    public static void columnGraph() {
+
+    }
+
+    public static void pointGraph(String graphName, String[] names, String[] params, int[][] values) {
+        initProperties();
+        BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2d = bufferedImage.createGraphics();
+        g2d.setColor(Color.white);
+        //ToDo: Check color and colors int array
+        g2d.fillRect(0, 0, width, height);
+
+
+    }
+
+    private static void getColor(String color){
+        int r = 0, g = 0, b = 0;
+        String cl = "";
 
 
 
-
-
+    }
 
 
 }
--- src/com/yabcompany/PropertiesScanner.java
+++ src/com/yabcompany/PropertiesScanner.java
@@ -1,6 +1,75 @@
 package com.yabcompany;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.Scanner;
+
+
 public class PropertiesScanner {
+    private static FileInputStream fis;
+    private static Properties property = new Properties();
+    private static Scanner scanner = null;
+    private static int width;
+    private static int height;
+    private static String bgColor;
+    private static String textColor;
+    private static String[] colors;
 
 
+    public static void setProperties() {
+        try {
+            fis = new FileInputStream("C:\\Users\\ramil\\Excel\\src\\com\\yabcompany\\config.properties");
+            int ans = checkProperties();
+            property.load(fis);
+            width = Integer.parseInt(property.getProperty("screen.width"));
+            height = Integer.parseInt(property.getProperty("screen.height"));
+            bgColor = property.getProperty("color.bd");
+            textColor = property.getProperty("color.text");
+            colors = new String[ans-4];
+            for (int i = 0; i < colors.length; i++){
+                String currentColor = "color." + (i+1);
+                colors[i] = property.getProperty(currentColor);
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Enter correct screen size");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static int checkProperties() {
+        int ans = 0;
+        try {
+            scanner = new Scanner(new File("C:\\Users\\ramil\\Excel\\src\\com\\yabcompany\\config.properties"));
+            while (scanner.hasNextLine()) {
+                ans++;
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Cant find properties file");
+        }
+        return ans;
+    }
+
+    public static int getWidth() {
+        return width;
+    }
+
+    public static int getHeight() {
+        return height;
+    }
+
+    public static String getBgColor() {
+        return bgColor;
+    }
+
+    public static String getTextColor() {
+        return textColor;
+    }
+
+    public static String[] getColors() {
+        return colors;
+    }
 }
