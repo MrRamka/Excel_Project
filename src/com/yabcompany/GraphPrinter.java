31ceee9d8ead5a2939c16035ffb32178d97fd414
MrRamka <41204824+mrramka@users.noreply.github.com>
2018-12-10 16:13:32 +0300

    yiiiiii

--- src/com/yabcompany/GraphPrinter.java
+++ src/com/yabcompany/GraphPrinter.java
@@ -2,6 +2,7 @@ package com.yabcompany;
 //Памагити! Рамиль опять какую-то непонятную хуйню написал :(
 
 import javax.imageio.ImageIO;
+import javax.sound.midi.Soundbank;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -9,18 +10,19 @@ import java.io.IOException;
 import java.util.Arrays;
 
 public class GraphPrinter {
-    protected static int width;
-    protected static int height;
-    protected static String bgColor;
-    protected static String textColor;
-    protected static String[] colors;
-    protected static int[] BG_COLOR;
-    protected static int[] TEXT_COLOR;
-    protected static int fontSize = 80;
+    private static int width;
+    private static int height;
+    private static String bgColor;
+    private static String textColor;
+    private static String[] colors;
+    private static int[] BG_COLOR;
+    private static int[] TEXT_COLOR;
+    private static int fontSize = 80;
+    private static int margin_left;
+    private static int margin_top;
+    private static int margin_right;
+    private static int margin_bottom;
 
-    public static String getBgColor() {
-        return bgColor;
-    }
 
     protected static void initProperties() {
         PropertiesScanner.setProperties();
@@ -31,6 +33,10 @@ public class GraphPrinter {
         colors = PropertiesScanner.getColors();
         BG_COLOR = getColor(bgColor);
         TEXT_COLOR = getColor(textColor);
+        margin_left = (int) (width * 0.15);
+        margin_top = (int) (height * 0.2);
+        margin_right = (int) (width * 0.85);
+        margin_bottom = (int) (height * 0.8);
     }
 
     public static void linearGraph() {
@@ -41,6 +47,7 @@ public class GraphPrinter {
 
     }
 
+
     public static void pointGraph(String graphName, String[] names, String[] params, int[][] values) {
         initProperties();
         BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
@@ -50,29 +57,34 @@ public class GraphPrinter {
         g2d.setColor(new Color(BG_COLOR[0], BG_COLOR[1], BG_COLOR[2]));
         g2d.fillRect(0, 0, width, height);
         g2d.setColor(new Color(TEXT_COLOR[0], TEXT_COLOR[1], TEXT_COLOR[2]));
-        g2d.drawString(graphName, (int) (width * 0.15), (int) (height * 0.2)); // Graph
-        g2d.setStroke(new BasicStroke(10));
-        g2d.drawLine((int) (width * 0.15), (int) (height * 0.8), (int) (width * 0.85), (int) (height * 0.8)); // X
-        //g2d.drawLine((int) (width * 0.15), (int) (height * 0.3), (int) (width * 0.15), (int) (height * 0.8)); // Y
+        g2d.drawString(graphName, margin_left, margin_top); // Graph
 
-        //Vertical lines
-        g2d.setStroke(new BasicStroke(1));
-        double step = height * 0.5 / 4;
-        for (int i = 0; i < 4; i++) {
-            g2d.drawLine((int) (width * 0.15), (int) (height * 0.3 + i * step), (int) (width * 0.85), (int) (height * 0.3 + i * step));
-        }
+        //Lines
+        drawLines(g2d);
 
         //Names
-        step = width * 0.7 / names.length;
-        font = new Font("Bahnschrift", Font.PLAIN, fontSize/4);
-        g2d.setFont(font);
-        for (int i = 0; i < names.length; i++) {
-            g2d.drawString(names[i], (int) (width * 0.15 + i * step + step/2), (int) (height * 0.85));
+        printNames(g2d, names);
 
+        //
+        int mx = maxValue(values);
+        System.out.println(mx);
+        int[] newColor = getColor(colors[1]);
+        double step = width * 0.7 / names.length;
+        g2d.setColor(new Color(newColor[0], newColor[1], newColor[2]));
+        g2d.setStroke(new BasicStroke(8));
+
+        for (int j = 0; j < params.length; j++) {
+            newColor = getColor(colors[j+1]);
+            g2d.setColor(new Color(newColor[0], newColor[1], newColor[2]));
+            for (int i = 0; i < names.length - 1; i++) {
+                g2d.drawLine((int) (margin_left + i * step + step / 2), margin_bottom - (int) (values[i][j] * 0.5 * height / mx),
+                        (int) (margin_left + (i + 1) * step + step / 2), margin_bottom - (int) (values[i + 1][j] * 0.5 * height / mx));
+
+            }
         }
 
-        g2d.dispose();
 
+        g2d.dispose();
 
         File file = new File(System.getProperty("user.home"), "Desktop\\image.png");
         try {
@@ -80,9 +92,10 @@ public class GraphPrinter {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        //saveToPng(bufferedImage);
     }
 
-    protected static int[] getColor(String color) {
+    private static int[] getColor(String color) {
         int[] currentColor = new int[3];
         try {
             currentColor[0] = Integer.valueOf(color.substring(1, 3), 16);
@@ -95,17 +108,47 @@ public class GraphPrinter {
     }
 
     private static int maxValue(int[][] values) {
-        int[][] arr = Arrays.copyOf(values, values.length);
-        Arrays.sort(arr);
-        int mx = arr[arr.length - 1][arr.length - 1];
+        int mx = values[0][0];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 2; j++) {
+                if (mx < values[i][j]) {
+                    mx = values[i][j];
+                }
+            }
+        }
         return mx;
     }
 
-    private static void roundMaxValue(){
+    private static void drawLines(Graphics2D g2d) {
+        g2d.setStroke(new BasicStroke(10));
+        g2d.drawLine(margin_left, margin_bottom, margin_right, margin_bottom); // X
+        //g2d.drawLine((int) (width * 0.15), (int) (height * 0.3), (int) (width * 0.15), (int) (height * 0.8)); // Y
 
-
+        //Vertical lines
+        g2d.setStroke(new BasicStroke(1));
+        double step = height * 0.5 / 4;
+        for (int i = 0; i < 4; i++) {
+            g2d.drawLine(margin_left, (int) (height * 0.3 + i * step), margin_right, (int) (height * 0.3 + i * step));
+        }
 
     }
 
+    private static void printNames(Graphics2D g2d, String[] names) {
+        double step = width * 0.7 / names.length;
+        Font font = new Font("Bahnschrift", Font.PLAIN, fontSize / 4);
+        g2d.setFont(font);
+        for (int i = 0; i < names.length; i++) {
+            g2d.drawString(names[i], (int) (margin_left + i * step + step / 2), (int) (height * 0.85));
 
+        }
+    }
+
+    private static void saveToPng(BufferedImage bufferedImage) {
+        File file = new File(System.getProperty("user.home"), "Desktop\\image.png");
+        try {
+            ImageIO.write(bufferedImage, "png", file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/com/yabcompany/Main.java
+++ src/com/yabcompany/Main.java
@@ -1,64 +1,15 @@
 package com.yabcompany;
 
-import com.yabcompany.Graphs.PointGraph;
 
-import java.io.*;
-import javax.imageio.ImageIO;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.util.Properties;
+public class Main {
 
-public class Main extends JFrame {
+    public static void main(String[] args) {
+        String graphName = "Points scored";
+        String[] names = {"Team 1", "Team 2", "Team 3", "Team 4",};
+        String[] param = {"Period 1", "Period 2"};
+        int[][] values = {{50, 89}, {36, 24}, {12, 37}, {38, 67}};
 
-        public static void main(String[] args) {
-            /*FileInputStream fis;
-            Properties property = new Properties();
-
-            try {
-                fis = new FileInputStream("C:\\Users\\ramil\\Excel\\src\\com\\yabcompany\\config.properties");
-                property.load(fis);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            int width = Integer.parseInt(property.getProperty("screen.width"));
-            int height = Integer.parseInt(property.getProperty("screen.height"));
-
-            // Constructs a BufferedImage of one of the predefined image types.
-            BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-
-            // Create a graphics which can be used to draw into the buffered image
-            Graphics2D g2d = bufferedImage.createGraphics();
-
-            // fill all the image with white
-            g2d.setColor(Color.white);
-            g2d.fillRect(0, 0, width, height);
-
-            // create a circle with black
-            g2d.setColor(Color.black);
-            g2d.fillOval(width/4, 0, height, height);
-
-            // create a string with yellow
-            g2d.setColor(Color.yellow);
-            g2d.drawString(property.getProperty("screen.height"), width/2, height/2);
-
-            // Disposes of this graphics context and releases any system resources that it is using.
-            g2d.dispose();
-
-            // Save as PNG
-            File file = new File("C:\\Users\\ramil\\Desktop\\image.png");
-            try {
-                ImageIO.write(bufferedImage, "png", file);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }*/
-            String graphName = "Points scored";
-            String[] names = {"Team 1", "Team 2","Team 3","Team 4",};
-            String[] param = {"Period 1", "Period 2"};
-            int[][] values = {{24, 89}, {36, 24}, {12, 37}, {38, 67}};
-
-            GraphPrinter.pointGraph(graphName,names,param,values);
-            System.out.println(System.getProperty("user.home"));
+        GraphPrinter.pointGraph(graphName, names, param, values);
 
     }
 }
