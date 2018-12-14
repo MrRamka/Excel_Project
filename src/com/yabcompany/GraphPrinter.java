8ec43a7fd467d45439806c85adc059aff76078dc
Rasim <rasim.2@bk.ru>
2018-12-14 11:30:18 +0300

    VertGraphDone (ny pochti)

--- src/com/yabcompany/GraphPrinter.java
+++ src/com/yabcompany/GraphPrinter.java
@@ -35,14 +35,48 @@ public class GraphPrinter {
         margin_top = (int) (height * 0.2);
         margin_right = (int) (width * 0.8);
         margin_bottom = (int) (height * 0.8);
-    }
 
-    public static void linearGraph() {
 
     }
 
-    public static void columnGraph() {
+    public static void vertColumnGraph(String graphName, String[] names, String[] params, int[][] values) {
+        initProperties();
+        checkNegativeNumbers(values, names.length, params.length);
+        BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2d = bufferedImage.createGraphics();
+        Font font = new Font("Bahnschrift", Font.PLAIN, fontSize);
+        g2d.setFont(font);
+        g2d.setColor(new Color(BG_COLOR[0], BG_COLOR[1], BG_COLOR[2]));
+        g2d.fillRect(0, 0, width, height);
+        g2d.setColor(new Color(TEXT_COLOR[0], TEXT_COLOR[1], TEXT_COLOR[2]));
+        g2d.drawString(graphName, margin_left, margin_top); // Graph
+        int mx = maxValue(values, names.length, params.length);
+        int[] newColor;
+        //Lines
+        drawLines(g2d, mx);
 
+        //Names
+        printNames(g2d, names);
+
+        double step = 1.0 * (margin_right - margin_left) / names.length;
+        String[] newColors = createRandomColors(colors);
+        newColor = getColor(newColors[0]);
+        /*g2d.setColor(new Color(newColor[0], newColor[1], newColor[2]));
+        g2d.fillRect((int)(margin_left + step/6), margin_bottom -(int)(values[0][0] * 0.5 * height / mx), (int)(step/3), (int)(values[0][0] * 0.5 * height / mx));
+        newColor = getColor(newColors[1]);
+        g2d.setColor(new Color(newColor[0], newColor[1], newColor[2]));
+        g2d.fillRect((int)(margin_left+step/6+step/3), margin_bottom -(int)(values[0][1] * 0.5 * height / mx), (int)(step/3), (int)(values[0][1] * 0.5 * height / mx));*/
+        for (int i = 0; i < params.length; i++) {
+            newColor = getColor(newColors[i]);
+            g2d.setColor(new Color(newColor[0], newColor[1], newColor[2]));
+            for (int j = 0; j < names.length; j++) {
+                g2d.fillRect((int) (step * (j + 1) + step / (params.length + 1) + (i) * step / (params.length + 1)), margin_bottom -(int) (margin_bottom *  0.01)- (int) (values[j][i] * 0.5 * height / mx), (int) (step / (params.length + 1)), (int) (values[j][i] * 0.5 * height / mx));
+            }
+        }
+
+
+        g2d.dispose();
+        saveToPng(bufferedImage);
     }
 
     /**
@@ -51,20 +85,19 @@ public class GraphPrinter {
      * @param params
      * @param values
      */
-
     public static void pointGraph(String graphName, String[] names, String[] params, int[][] values) {
         initProperties();
         checkNegativeNumbers(values, names.length, params.length);
         BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         Graphics2D g2d = bufferedImage.createGraphics();
-        int mx = maxValue(values, names.length, params.length);
-        int[] newColor;
         Font font = new Font("Bahnschrift", Font.PLAIN, fontSize);
         g2d.setFont(font);
         g2d.setColor(new Color(BG_COLOR[0], BG_COLOR[1], BG_COLOR[2]));
         g2d.fillRect(0, 0, width, height);
         g2d.setColor(new Color(TEXT_COLOR[0], TEXT_COLOR[1], TEXT_COLOR[2]));
         g2d.drawString(graphName, margin_left, margin_top); // Graph
+        int mx = maxValue(values, names.length, params.length);
+        int[] newColor;
 
         //Lines
         drawLines(g2d, mx);
@@ -80,12 +113,12 @@ public class GraphPrinter {
             newColor = getColor(newColors[j]);
             g2d.setColor(new Color(newColor[0], newColor[1], newColor[2]));
             for (int i = 0; i < names.length - 1; i++) {
-                g2d.drawLine((int) (margin_left + i * step + step / 2 + step/8), margin_bottom - (int) (values[i][j] * 0.5 * height / mx),
-                        (int) (margin_left + (i + 1) * step + step / 2 + step/8), margin_bottom - (int) (values[i + 1][j] * 0.5 * height / mx));
+                g2d.drawLine((int) (margin_left + i * step + step / 2 + step / 8), margin_bottom - (int) (values[i][j] * 0.5 * height / mx),
+                        (int) (margin_left + (i + 1) * step + step / 2 + step / 8), margin_bottom - (int) (values[i + 1][j] * 0.5 * height / mx));
             }
-            g2d.fillRect((int)(margin_right + margin_right * 0.03), (int)(height * 0.3 + j * height * 0.05), 50,25);
+            g2d.fillRect((int) (margin_right + margin_right * 0.03), (int) (height * 0.3 + j * height * 0.05), 50, 25);
             g2d.setColor(new Color(TEXT_COLOR[0], TEXT_COLOR[1], TEXT_COLOR[2]));
-            g2d.drawString(params[j],(int)(margin_right + margin_right * 0.07), (int)(height * 0.3 + j * height * 0.05) + 20);
+            g2d.drawString(params[j], (int) (margin_right + margin_right * 0.07), (int) (height * 0.3 + j * height * 0.05) + 20);
         }
 
         //g2d.fillArc(0,0,250,250,0,60);
@@ -133,7 +166,7 @@ public class GraphPrinter {
         for (int i = 0; i < 4; i++) {
             g2d.drawLine(margin_left, (int) (height * 0.3 + i * step), margin_right, (int) (height * 0.3 + i * step));
             s += (int) (mx * 0.25 * (4 - i));
-            g2d.drawString(s, (int)(margin_left * 0.9), (int) (height * 0.3 + i * step));
+            g2d.drawString(s, (int) (margin_left * 0.8), (int) (height * 0.3 + i * step));
             s = "";
         }
 
@@ -144,13 +177,13 @@ public class GraphPrinter {
         Font font = new Font("Bahnschrift", Font.PLAIN, fontSize / 4);
         g2d.setFont(font);
         for (int i = 0; i < names.length; i++) {
-            g2d.drawString(names[i], (int) (margin_left + i * step + step / 2), (int)(margin_bottom + margin_bottom * 0.07));
+            g2d.drawString(names[i], (int) (margin_left + i * step + step/3), (int) (margin_bottom + margin_bottom * 0.07));
 
         }
     }
 
     private static void saveToPng(BufferedImage bufferedImage) {
-        File file = new File(System.getProperty("user.home"), "Desktop\\image.png");
+        File file = new File(System.getProperty("user.home"), "Desktop\\Graph.png");
         try {
             ImageIO.write(bufferedImage, "png", file);
         } catch (IOException e) {
@@ -158,26 +191,39 @@ public class GraphPrinter {
         }
     }
 
-    private static String[] createRandomColors(String[] colors){
-        String [] newColorArr = new String[colors.length];
-        String [] copyColors = Arrays.copyOf(colors, colors.length);
+    private static String[] createRandomColors(String[] colors) {
+        String[] newColorArr = new String[colors.length];
+        String[] copyColors = Arrays.copyOf(colors, colors.length);
         int pos;
         for (int i = 0; i < colors.length - 2; i++) {
-            pos = (int)(Math.random() * (colors.length-(i+1)));
+            pos = (int) (Math.random() * (colors.length - (i + 1)));
             newColorArr[i] = copyColors[pos];
-            copyColors[pos] = copyColors[colors.length -1];
+            copyColors[pos] = copyColors[colors.length - 1];
         }
         return newColorArr;
     }
 
-    private static void checkNegativeNumbers(int[][] values,int namesLen,int paramLen){
+    private static void checkNegativeNumbers(int[][] values, int namesLen, int paramLen) {
         for (int i = 0; i < namesLen; i++) {
             for (int j = 0; j < paramLen; j++) {
                 if (values[i][j] < 0) {
-                    throw new IllegalArgumentException("Problems?)");
+                    throw new IllegalArgumentException("Values can't be negative(<0)");
                 }
             }
         }
+    }
+
+    protected static void chlen(String graphName, String[] names, String[] params, int[][] values) {
+        initProperties();
+        checkNegativeNumbers(values, names.length, params.length);
+        BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2d = bufferedImage.createGraphics();
+        Font font = new Font("Bahnschrift", Font.PLAIN, fontSize);
+        g2d.setFont(font);
+        g2d.setColor(new Color(BG_COLOR[0], BG_COLOR[1], BG_COLOR[2]));
+        g2d.fillRect(0, 0, width, height);
+        g2d.setColor(new Color(TEXT_COLOR[0], TEXT_COLOR[1], TEXT_COLOR[2]));
+        g2d.drawString(graphName, margin_left, margin_top); // Graph
     }
 
 
--- src/com/yabcompany/Main.java
+++ src/com/yabcompany/Main.java
@@ -5,11 +5,12 @@ public class Main {
 
     public static void main(String[] args) {
         String graphName = "Points scored";
-        String[] names = {"Team 1", "Team 2", "Team 3", "Team 4","Team 5"};
+        String[] names = {"Team A", "Team B", "Team C", "Team D","Team E"};
         String[] param = {"Period 1", "Period 2","Period 3"};
         int[][] values = {{20, 10, 40}, {25, 50, 60}, {75, 30,90}, {40, 60, 20  }, {75,40,100}};
 
-        GraphPrinter.pointGraph(graphName, names, param, values);
+        GraphPrinter.vertColumnGraph(graphName, names, param, values);
+
 
     }
 }
